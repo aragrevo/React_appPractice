2d3520616700375964223bd875161515a0688d8e
aragrevo <eduver_san@hotmail.com>
2021-01-15 19:22:40 -0500

    use Memo

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useMemo } from 'react';
 
 import { Row, Col } from 'antd';
 import { Button, Tooltip } from 'antd';
 import { Card } from 'antd';
 import { SmileTwoTone, HeartTwoTone, HeartOutlined } from '@ant-design/icons';
+import { Input, Space } from 'antd';
 
 const { Meta } = Card;
+const { Search } = Input;
 
 const initialState = {
   favorites: [],
@@ -33,6 +35,7 @@ const favoriteReducer = (state, action) => {
 export const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+  const [search, setSearch] = useState('');
 
   const getCharacters = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character/');
@@ -57,10 +60,35 @@ export const Characters = () => {
     return favorites.favorites.some((fav) => fav.id === character.id);
   };
 
+  const onSearch = (event) => {
+    const text = event.target.value;
+    if (text || text === '') {
+      setSearch(text.toLowerCase());
+      return;
+    }
+    setSearch(event.toLowerCase());
+  };
+
+  const filteredUsers = useMemo(
+    () =>
+      characters.filter((user) => {
+        return user.name.toLowerCase().includes(search);
+      }),
+    [characters, search]
+  );
+
   return (
     <>
+      <Search />
+      <Search />
+      <Search
+        placeholder='input search text'
+        onSearch={onSearch}
+        onChange={onSearch}
+      />
+
       <Row gutter={[16, 16]} justify='space-around'>
-        {characters.map((character) => (
+        {filteredUsers.map((character) => (
           <Col span={4} key={character.id}>
             <Card
               hoverable
